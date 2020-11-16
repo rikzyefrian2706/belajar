a49c8ab80e361a74bc352bda3edb52a0805cb4ee
rizkyefrian <rizkyefrian7@gmail.com>
2020-11-16 15:22:32 +0700

    first commit

---
+++ app/Http/Controllers/LaundryPriceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LaundryPrice;
+use Illuminate\Http\Request;
+
+class LaundryPriceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LaundryPrice  $laundryPrice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LaundryPrice $laundryPrice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LaundryPrice  $laundryPrice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LaundryPrice $laundryPrice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LaundryPrice  $laundryPrice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LaundryPrice $laundryPrice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LaundryPrice  $laundryPrice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LaundryPrice $laundryPrice)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/LaundryTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LaundryType;
+use Illuminate\Http\Request;
+
+class LaundryTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LaundryType  $laundryType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LaundryType $laundryType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LaundryType  $laundryType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LaundryType $laundryType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LaundryType  $laundryType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LaundryType $laundryType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LaundryType  $laundryType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LaundryType $laundryType)
+    {
+        //
+    }
+}
